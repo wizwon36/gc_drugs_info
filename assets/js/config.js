4a483cb79e82bc7dab5b97c5e4cb950ce70691c5
wizwon36 <wizwon36@gmail.com>
2026-03-19 07:56:27 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,5 @@
 const API_BASE = 'https://script.google.com/macros/s/AKfycbyrR40UQWw2FwlU6tiPplAfAQqzE1d3JBvmamNvxNRymOg1vH9IP6fFZ2qDm5hxNFqwmg/exec';
+const UI_LOADING_DELAY = 750;
 
 const state = {
   currentMode: 'patient',
@@ -18,6 +19,7 @@ const state = {
   isSearching: false,
   isInitializing: true,
   suppressBlurHide: false,
+  autocompleteLoadingKeyword: '',
 
   adminTapCount: 0,
   adminTapTimer: null,
